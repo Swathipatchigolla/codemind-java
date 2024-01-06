ab9942b2271702ba9684c01c152733924e2ca197
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2024-01-06 11:12:11 +0530

    import java.util.*; public class Class{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int p=2*(a+b);         int w=p*c;         System.out.print(w);     } }

---
+++ Frames.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Class{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int p=2*(a+b);
+        int w=p*c;
+        System.out.print(w);
+    }
+}
