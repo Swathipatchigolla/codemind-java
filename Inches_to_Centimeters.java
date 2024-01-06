bdd9a429abccee8cf88da259251582cf9ecc3022
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2024-01-06 14:19:23 +0530

    import java.util.*; public class Class{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int i=sc.nextInt();         System.out.printf("%.2f",i*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Class{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        System.out.printf("%.2f",i*2.54);
+    }
+}
