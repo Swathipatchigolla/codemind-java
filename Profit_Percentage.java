e75143f136608956296e2622bbb5ac6f0351fbe9
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2024-01-06 11:22:49 +0530

    import java.util.*; public class Class{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double c=Math.abs(a-b)/(a*1.0);         System.out.printf("%.2f",c*100);     } }

---
+++ Profit_Percentage.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Class{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=Math.abs(a-b)/(a*1.0);
+        System.out.printf("%.2f",c*100);
+    }
+}
