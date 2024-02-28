0923b649a0a7dde72518c79d7f5b5f47f9546e04
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2024-02-28 10:20:14 +0530

    import java.util.Scanner; public class Solution{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int q=sc.nextInt();         int p=sc.nextInt();         System.out.println(p*q);     } }

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int q=sc.nextInt();
+        int p=sc.nextInt();
+        System.out.println(p*q);
+    }
+}
