b46f25843cc11823a057f96c1855511036b436ac
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2024-03-07 11:16:49 +0530

    import java.util.Scanner; public class Solution{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x,y;         x=sc.nextInt();         y=sc.nextInt();         if(x>=2*y){             System.out.println("YES");         }         else System.out.println("NO");     } }

---
+++ Good_Investment__or__Not.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        if(x>=2*y){
+            System.out.println("YES");
+        }
+        else System.out.println("NO");
+    }
+}
